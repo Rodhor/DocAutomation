023df87b724076880f9d7867c9925057cb9bf793
Chris Nielsen <chris.rahbek.nielsen@gmail.com>
2024-04-20 15:43:28 +0200

    Tag screen mostly done - adding widget to scrollview was not be implementet

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.tab import MDTabsBase
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.boxlayout import MDBoxLayout
 
 
 class IndividualTab(MDTabsBase, MDFloatLayout):
     pass
+
+
+class TagListItem(MDBoxLayout):
+    tag = ""
+    meaning = ""
+    description = ""
 
 
 class Screen1(MDScreen):
@@ -32,10 +39,31 @@ class MainApp(MDApp):
         return sm
 
     def on_start(self):
+
         for screen in self.root.screens:
             if screen.name == "screen1":
                 for i in range(5):
                     screen.ids.tabs.add_widget(IndividualTab(title=f"test{i}"))
+
+    def change_tags(self):
+        self.root.current = "screen2"
+
+    def change_categories(self):
+        self.root.current = "screen3"
+
+    def change_templates_path(self):
+        pass
+
+    def add_new_tag(self, tag_text, tag_meaning, tag_description):
+        new_tag = TagListItem(
+            text=tag_text, secondary_text=tag_meaning, tertiary_text=tag_description
+        )
+        screen = self.root.screens[1]
+        screen.ids.taglist.add_widget(new_tag)
+        screen.ids.tag_field.text = ""
+        screen.ids.meaning_field.text = ""
+        screen.ids.description_field.text = ""
+        screen.ids.tag_field.focus = True
 
 
 if __name__ == "__main__":

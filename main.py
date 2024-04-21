15c57029a1ca9410acdb683c7bbf62bd9aae09ed
Chris Nielsen <chris.rahbek.nielsen@gmail.com>
2024-04-21 12:23:59 +0200

    Implemented TagSettingsView and tag type handling Added icons and error messages for tag and type validation Implemented main app theme Future changes to theme may be made

--- main.py
+++ main.py
@@ -5,65 +5,70 @@ from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.tab import MDTabsBase
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.floatlayout import MDFloatLayout
-from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.list import TwoLineAvatarIconListItem
 
 
 class IndividualTab(MDTabsBase, MDFloatLayout):
     pass
 
 
-class TagListItem(MDBoxLayout):
-    tag = ""
-    meaning = ""
-    description = ""
-
-
-class Screen1(MDScreen):
+class TagListItem(TwoLineAvatarIconListItem):
     pass
 
 
-class Screen2(MDScreen):
+class MainScreen(MDScreen):
     pass
 
 
-class Screen3(MDScreen):
+class TagSettingsScreen(MDScreen):
+    pass
+
+
+class CategorySettingsScreen(MDScreen):
     pass
 
 
 class MainApp(MDApp):
     def build(self):
+        # Setting the theme to be a nice shade of orange with white and black text
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Amber"
+        self.theme_cls.primary_hue = "900"
+
+        # Defining and loading the screens, adding them to the screenmanager
         sm = MDScreenManager()
-        sm.add_widget(Screen1(name="screen1"))
-        sm.add_widget(Screen2(name="screen2"))
-        sm.add_widget(Screen3(name="screen3"))
+        sm.add_widget(MainScreen(name="MainScreen"))
+        sm.add_widget(TagSettingsScreen(name="TagSettingsScreen"))
+        sm.add_widget(CategorySettingsScreen(name="CategorySettingsScreen"))
         return sm
 
     def on_start(self):
 
         for screen in self.root.screens:
-            if screen.name == "screen1":
+            if screen.name == "MainScreen":
                 for i in range(5):
                     screen.ids.tabs.add_widget(IndividualTab(title=f"test{i}"))
 
     def change_tags(self):
-        self.root.current = "screen2"
+        self.root.current = "TagSettingsScreen"
 
     def change_categories(self):
-        self.root.current = "screen3"
+        self.root.current = "CategorySettingsScreen"
 
     def change_templates_path(self):
         pass
 
-    def add_new_tag(self, tag_text, tag_meaning, tag_description):
-        new_tag = TagListItem(
-            text=tag_text, secondary_text=tag_meaning, tertiary_text=tag_description
-        )
-        screen = self.root.screens[1]
-        screen.ids.taglist.add_widget(new_tag)
-        screen.ids.tag_field.text = ""
-        screen.ids.meaning_field.text = ""
-        screen.ids.description_field.text = ""
-        screen.ids.tag_field.focus = True
+    def add_new_tag(self, tag_text, tag_type, tag_description):
+        if not tag_text == "" and not tag_type == "":
+            new_tag = TagListItem(
+                id=tag_text, text=tag_type, secondary_text=tag_description
+            )
+            screen = self.root.screens[1]
+            screen.ids.taglist.add_widget(new_tag)
+            screen.ids.tag_field.text = ""
+            screen.ids.type_field.text = ""
+            screen.ids.description_field.text = ""
+            screen.ids.tag_field.focus = True
 
 
 if __name__ == "__main__":
